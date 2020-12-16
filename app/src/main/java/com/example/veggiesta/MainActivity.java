966c731eb7870c68713bce2194c88d5b66c95b06
Brian Kibet <brian.kipketer@strathmore.edu>
2020-12-16 15:37:02 +0300

    First Commit

---
+++ app/src/main/java/com/example/veggiesta/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.veggiesta;
+
+import android.os.Handler;
+import android.view.WindowManager;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+    }
+}
