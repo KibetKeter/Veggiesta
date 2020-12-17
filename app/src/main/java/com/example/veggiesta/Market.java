001d014bde0b6026f9d313b945d17cdd864754f5
Brian Kibet <brian.kipketer@strathmore.edu>
2020-12-17 19:01:18 +0300

    Push Market

--- app/src/main/java/com/example/veggiesta/Market.java
+++ app/src/main/java/com/example/veggiesta/Market.java
@@ -2,13 +2,15 @@ package com.example.veggiesta;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class Market extends AppCompatActivity {
 
     RecyclerView recyclerview;
 
-        String s1[] ,s2[];
+        String s1[];
+        int s2[];
         int images[] = {R.drawable.ic_friedrice,R.drawable.ic_lentilsoup,R.drawable.ic_falafel,R.drawable.ic_vegetariantacos,R.drawable.ic_peanutnoodles
         , R.drawable.ic_vegsoup, R.drawable.ic_lassagna, R.drawable.ic_pumpkinsoup};
 
@@ -21,5 +23,10 @@ public class Market extends AppCompatActivity {
         recyclerview = findViewById(R.id.recyclerview);
 
         s1 = getResources().getStringArray(R.array.Veggie_Meals);
+        s2 = getResources().getIntArray(R.array.Price);
+
+        MyAdapter myadapter = new MyAdapter(this, s1,images,s2);
+        recyclerview.setAdapter(myadapter);
+        recyclerview.setLayoutManager(new LinearLayoutManager(this));
     }
 }
--- app/src/main/java/com/example/veggiesta/MyAdapter.java
+++ app/src/main/java/com/example/veggiesta/MyAdapter.java
@@ -1,12 +1,63 @@
 package com.example.veggiesta;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder>
 {
+    String data1[];
+    int img[];
+    Context context;
+    int Price[];
+
+    public MyAdapter(Context ct, String s1[], int images[], int s2[])
+        {
+            context = ct;
+            data1 = s1;
+            img = images;
+            Price = s2;
+        }
+    @NonNull
+    @org.jetbrains.annotations.NotNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull @org.jetbrains.annotations.NotNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.my_row,parent,false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull @org.jetbrains.annotations.NotNull MyAdapter.MyViewHolder holder, int position) {
+        holder.mytext1.setText(data1[position]);
+        holder.mytext2.setText(Price[position]);
+        holder.myImage.setImageResource(img[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return data1.length;
+    }
+
     public class MyViewHolder extends RecyclerView.ViewHolder
         {
+            TextView mytext1;
+            TextView mytext2;
+            ImageView myImage;
 
+            public MyViewHolder(@NonNull View itemView)
+                {
+                    super(itemView);
+                    mytext1 = itemView.findViewById(R.id.foodname);
+                    mytext2 = itemView.findViewById(R.id.price);
+                    myImage = itemView.findViewById(R.id.foodpicture);
+
+                }
 
         }
 }
