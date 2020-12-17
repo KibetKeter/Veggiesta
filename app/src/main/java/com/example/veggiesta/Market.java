24ac312714d27b70b49c4c5228f7047312f58bf9
Brian Kibet <brian.kipketer@strathmore.edu>
2020-12-17 15:58:46 +0300

    Started working on the Market

--- app/src/main/java/com/example/veggiesta/Market.java
+++ app/src/main/java/com/example/veggiesta/Market.java
@@ -2,12 +2,24 @@ package com.example.veggiesta;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class Market extends AppCompatActivity {
+
+    RecyclerView recyclerview;
+
+        String s1[] ,s2[];
+        int images[] = {R.drawable.ic_friedrice,R.drawable.ic_lentilsoup,R.drawable.ic_falafel,R.drawable.ic_vegetariantacos,R.drawable.ic_peanutnoodles
+        , R.drawable.ic_vegsoup, R.drawable.ic_lassagna, R.drawable.ic_pumpkinsoup};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_market);
+
+
+        recyclerview = findViewById(R.id.recyclerview);
+
+        s1 = getResources().getStringArray(R.array.Veggie_Meals);
     }
 }
---
+++ app/src/main/java/com/example/veggiesta/MyAdapter.java
@@ -0,0 +1,12 @@
+package com.example.veggiesta;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder>
+{
+    public class MyViewHolder extends RecyclerView.ViewHolder
+        {
+
+
+        }
+}
