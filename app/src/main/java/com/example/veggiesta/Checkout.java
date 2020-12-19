4726878457551fee3f6cde49da706c403bfefae8
Brian Kibet <brian.kipketer@strathmore.edu>
2020-12-19 16:35:08 +0300

    Completed the necessary pages that need to be worked on

---
+++ app/src/main/java/com/example/veggiesta/Checkout.java
@@ -0,0 +1,13 @@
+package com.example.veggiesta;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Checkout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_checkout);
+    }
+}
